a4592ca932c6262670226585ab46286ec84e9eba
James Skett <jamesskett31@gmail.com>
2017-11-02 15:16:46 +0000

    Updated mesh class
    added code to compile shaders and functions to get and set mesh properties

--- CGP600 AE2/Mesh.cpp
+++ CGP600 AE2/Mesh.cpp
@@ -6,11 +6,30 @@ Mesh::Mesh(ID3D11Device* D3D11Device, ID3D11DeviceContext* ImmediateContext)
 {
 	m_pD3D11Device = D3D11Device;
 	m_pImmediateContext = ImmediateContext;
+
+	m_x = 0.0f;
+	m_y = 0.0f;
+	m_z = 0.0f;
+	m_xangle = 0.0f;
+	m_yangle = 0.0f;
+	m_zangle = 0.0f;
+	m_scale = 0.0f;
 }
 
 Mesh::~Mesh()
 {
+	delete m_pObject;
+	m_pObject = nullptr;
 
+	if (m_pInputLayout)  m_pInputLayout->Release();
+	if (m_pVShader) m_pVShader->Release();
+	if (m_pPShader)  m_pPShader->Release();
+	//if (m_pTexture0)     m_pTexture0->Release();
+	//if (m_pSampler0)     m_pSampler0->Release();
+
+	if (m_pConstantBuffer)  m_pConstantBuffer->Release();
+	//if (m_pImmediateContext) m_pImmediateContext->Release();
+	//if (m_pD3D11Device)        m_pD3D11Device->Release();
 }
 
 int Mesh::LoadObjModel(char* fileName)
@@ -18,10 +37,27 @@ int Mesh::LoadObjModel(char* fileName)
 	
 	m_pObject = new ObjFileModel(fileName, m_pD3D11Device, m_pImmediateContext);
 
+	if (m_pObject->filename == "FILE NOT LOADED") return S_FALSE;
+
+	HRESULT hr = S_OK;
+	// Create constant buffer
+	D3D11_BUFFER_DESC constant_buffer_desc;
+	ZeroMemory(&constant_buffer_desc, sizeof(constant_buffer_desc));
+
+	constant_buffer_desc.Usage = D3D11_USAGE_DEFAULT;	// Can use UpdateSubresource() to update
+	constant_buffer_desc.ByteWidth = 64;  // Must Be a multiple of 16, calculate from CB struct
+	constant_buffer_desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER; // Use as a constant buffer
+
+	hr = m_pD3D11Device->CreateBuffer(&constant_buffer_desc, NULL, &m_pConstantBuffer);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
 
 	// Load and compile pixel and vertex shaders - use vs_5_0 to target DX11 hardware only
 	ID3DBlob *VS, *PS, *error;
-	/*hr = D3DX11CompileFromFile("text2d_shaders.hlsl", 0, 0, "TextVS", "vs_4_0", 0, 0, 0, &VS, &error, 0);
+	hr = D3DX11CompileFromFile("model_shaders.hlsl", 0, 0, "ModelVS", "vs_4_0", 0, 0, 0, &VS, &error, 0);
 
 	if (error != 0)
 	{
@@ -30,13 +66,168 @@ int Mesh::LoadObjModel(char* fileName)
 		if (FAILED(hr))exit(0);
 	}
 
-	hr = D3DX11CompileFromFile("text2d_shaders.hlsl", 0, 0, "TextPS", "ps_4_0", 0, 0, 0, &PS, &error, 0);
+	hr = D3DX11CompileFromFile("model_shaders.hlsl", 0, 0, "ModelPS", "ps_4_0", 0, 0, 0, &PS, &error, 0);
 
 	if (error != 0)
 	{
 		OutputDebugStringA((char*)error->GetBufferPointer());
 		error->Release();
 		if (FAILED(hr)) exit(0);
-	}*/
+	}
+
+	// Create shader objects
+	hr = m_pD3D11Device->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &m_pVShader);
+	if (FAILED(hr)) exit(0);
+
+	hr = m_pD3D11Device->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &m_pPShader);
+	if (FAILED(hr)) exit(0);
+
+	D3D11_INPUT_ELEMENT_DESC iedesc[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0,    0,                            D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,    D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,       0,    D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT,    0,    D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+
+	};
+
+	hr = m_pD3D11Device->CreateInputLayout(iedesc, ARRAYSIZE(iedesc), VS->GetBufferPointer(), VS->GetBufferSize(), &m_pInputLayout);
+
+	if (FAILED(hr)) exit(0);
+
+	m_pImmediateContext->IASetInputLayout(m_pInputLayout);
+
+
 	return 0;
 }
+
+void Mesh::Draw(XMMATRIX* view, XMMATRIX* projection)
+{
+	XMMATRIX world;
+
+	world = XMMatrixRotationX(m_xangle) * XMMatrixRotationY(m_yangle) * XMMatrixRotationZ(m_zangle);
+	world *= XMMatrixScaling(m_scale, m_scale, m_scale);
+	world *= XMMatrixTranslation(m_x, m_y, m_z);
+
+	MODEL_CONSTANT_BUFFER model_cb_values;
+	model_cb_values.WorldViewProjection = world * (*view) * (*projection);
+
+	// upload the new values for the constant buffer
+	m_pImmediateContext->UpdateSubresource(m_pConstantBuffer, 0, 0, &model_cb_values, 0, 0);
+
+	m_pImmediateContext->VSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+
+	m_pImmediateContext->VSSetShader(m_pVShader, 0, 0);
+	m_pImmediateContext->PSSetShader(m_pPShader, 0, 0);
+	m_pImmediateContext->IASetInputLayout(m_pInputLayout);
+
+	m_pObject->Draw();
+
+}
+
+void Mesh::SetXPos(float x_pos)
+{
+	m_x = x_pos;
+}
+
+void Mesh::SetYPos(float y_pos)
+{
+	m_y = y_pos;
+}
+
+void Mesh::SetZPos(float z_pos)
+{
+	m_z = z_pos;
+}
+
+void Mesh::SetXAngle(float x_angle)
+{
+	m_xangle = x_angle;
+}
+
+void Mesh::SetYAngle(float y_angle)
+{
+	m_yangle = y_angle;
+}
+
+void Mesh::SetZAngle(float z_angle)
+{
+	m_zangle = z_angle;
+}
+
+void Mesh::SetScale(float scale)
+{
+	m_scale = scale;
+}
+
+float Mesh::GetXPos()
+{
+	return m_x;
+}
+
+float Mesh::GetYPos()
+{
+	return m_y;
+}
+
+float Mesh::GetZPos()
+{
+	return m_z;
+}
+
+float Mesh::GetXAngle()
+{
+	return m_xangle;
+}
+
+float Mesh::GetYAndle()
+{
+	return m_yangle;
+}
+
+float Mesh::GetZAngle()
+{
+	return m_zangle;
+}
+
+float Mesh::GetScale()
+{
+	return m_scale;
+}
+
+void Mesh::UpdateXPos(float distance)
+{
+	m_x += distance;
+}
+
+void Mesh::UpdateYPos(float distance)
+{
+	m_y += distance;
+}
+
+void Mesh::UpdateZPos(float distance)
+{
+	m_z += distance;
+}
+
+void Mesh::UpdateXAngle(float angle)
+{
+	m_xangle += angle;
+}
+
+void Mesh::UpdateYAngle(float angle)
+{
+	m_yangle += angle;
+}
+
+void Mesh::UpdateZAngle(float angle)
+{
+	m_zangle += angle;
+}
+
+void Mesh::UpdateScale(float scale)
+{
+	m_scale += scale;
+}
+
+
--- CGP600 AE2/Mesh.h
+++ CGP600 AE2/Mesh.h
@@ -9,17 +9,49 @@ using namespace DirectX;
 #include <vector>
 #include "objfilemodel.h"
 
+struct MODEL_CONSTANT_BUFFER
+{
+	XMMATRIX WorldViewProjection;
+};
+
 class Mesh
 {
 public:
 	Mesh(ID3D11Device* D3D11Device, ID3D11DeviceContext* ImmediateContext);
 	~Mesh();
 
-	std::vector <XMFLOAT3> verticiesPos;
-	std::vector <XMFLOAT4> verticiesCol;
-	std::vector <XMFLOAT2> verticiesUV;
-
 	int LoadObjModel(char* fileName);
+
+	void Draw(XMMATRIX* view, XMMATRIX* projection);
+
+	//Setters
+	void SetXPos(float x_pos);
+	void SetYPos(float y_pos);
+	void SetZPos(float z_pos);
+	void SetXAngle(float x_angle);
+	void SetYAngle(float y_angle);
+	void SetZAngle(float z_angle);
+	void SetScale(float scale);
+
+	//Getters
+	float GetXPos();
+	float GetYPos();
+	float GetZPos();
+	float GetXAngle();
+	float GetYAndle();
+	float GetZAngle();
+	float GetScale();
+
+	//use these to move scale and rotate object
+	void UpdateXPos(float distance);
+	void UpdateYPos(float distance);
+	void UpdateZPos(float distance);
+	void UpdateXAngle(float angle);
+	void UpdateYAngle(float angle);
+	void UpdateZAngle(float angle);
+	void UpdateScale(float scale);
+
+
 
 private:
 	ID3D11Device*        m_pD3D11Device;
@@ -28,7 +60,7 @@ private:
 	ObjFileModel*		 m_pObject;
 	ID3D11VertexShader*  m_pVShader;
 	ID3D11PixelShader*	 m_pPShader;
-	ID3D11InputLayout*	 m_InputLayout;
+	ID3D11InputLayout*	 m_pInputLayout;
 	ID3D11Buffer*		 m_pConstantBuffer;
 
 	float m_x, m_y, m_z;
