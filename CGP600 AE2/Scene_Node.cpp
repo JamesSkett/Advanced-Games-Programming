00c504c7a28547e849c9bf36d7a3c17e4fb6630f
James Skett <jamesskett31@gmail.com>
2017-11-16 16:32:03 +0000

    Added scene manager class

---
+++ CGP600 AE2/Scene_Node.cpp
@@ -0,0 +1,127 @@
+#include "Scene_Node.h"
+
+
+
+Scene_Node::Scene_Node()
+{
+	m_pModel = NULL;
+
+	m_x = 0.0f; m_y = 0.0f; m_z = 5.0f;
+	m_xangle = 0.0f; m_yangle = 0.0f; m_zangle = 0.0f;
+	m_scale = 1.0f;
+}
+
+
+Scene_Node::~Scene_Node()
+{
+}
+
+//Getters
+float Scene_Node::GetXPos()
+{
+	return m_x;
+}
+float Scene_Node::GetYPos()
+{
+	return m_y;
+}
+float Scene_Node::GetZPos()
+{
+	return m_z;
+}
+float Scene_Node::GetXAngle()
+{
+	return m_xangle;
+}
+float Scene_Node::GetYAngle()
+{
+	return m_yangle;
+}
+float Scene_Node::GetZAngle()
+{
+	return m_zangle;
+}
+
+//Setters
+void Scene_Node::SetXPos(float x)
+{
+	m_x = x;
+}
+void Scene_Node::SetYPos(float y)
+{
+	m_y = y;
+}
+void Scene_Node::SetZPos(float z)
+{
+	m_z = z;
+}
+void Scene_Node::SetXAngle(float xAngle)
+{
+	m_xangle = xAngle;
+}
+void Scene_Node::SetYAngle(float yAngle)
+{
+	m_yangle = yAngle;
+}
+void Scene_Node::SetZAngle(float zAngle)
+{
+	m_zangle = zAngle;
+}
+
+void Scene_Node::SetModel(Mesh * mesh)
+{
+	m_pModel = mesh;
+
+
+
+}
+
+void Scene_Node::AddChildNode(Scene_Node * n)
+{
+	m_children.push_back(n);
+}
+
+bool Scene_Node::DetachNode(Scene_Node * n)
+{
+	for (int i = 0; i < m_children.size(); i++)
+	{
+		if (n == m_children[i])
+		{
+			m_children.erase(m_children.begin() + i);
+			return true;
+		}
+
+		if (m_children[i]->DetachNode(n) == true) return true;
+	}
+	return false;
+}
+
+void Scene_Node::Execute(XMMATRIX * world, XMMATRIX * view, XMMATRIX * projection)
+{
+	m_scale = 0.2f;
+	//m_z = 5;
+
+	XMMATRIX local_world = DirectX::XMMatrixIdentity();
+
+	local_world = DirectX::XMMatrixRotationX(XMConvertToRadians(m_xangle));
+	local_world *= DirectX::XMMatrixRotationY(XMConvertToRadians(m_yangle));
+	local_world *= DirectX::XMMatrixRotationZ(XMConvertToRadians(m_zangle));
+
+	local_world *= DirectX::XMMatrixScaling(m_scale, m_scale, m_scale);
+
+	local_world *= DirectX::XMMatrixTranslation(m_x, m_y, m_z);
+
+	//passed in world matrix contains concatenated transformations of all 
+	//parent nodes so that this nodes transformations are relative to those
+	local_world *= *world;
+
+	// only draw if there is a model attached
+	if (m_pModel) m_pModel->Draw(&local_world, view, projection);
+
+	// traverse all child nodes, passing in the concatenated world matrix
+	for (int i = 0; i< m_children.size(); i++)
+	{
+		m_children[i]->Execute(&local_world, view, projection);
+	}
+
+}
---
+++ CGP600 AE2/Scene_Node.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <dxerr.h>
+#include <dinput.h>
+#include <vector>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+#include "Mesh.h"
+
+class Scene_Node
+{
+public:
+	Scene_Node();
+	~Scene_Node();
+
+	//Getters
+	float GetXPos();
+	float GetYPos();
+	float GetZPos();
+	float GetXAngle();
+	float GetYAngle();
+	float GetZAngle();
+
+	//Setters
+	void SetXPos(float x);
+	void SetYPos(float y);
+	void SetZPos(float z);
+	void SetXAngle(float xAngle);
+	void SetYAngle(float yAngle);
+	void SetZAngle(float zAngle);
+
+	void SetModel(Mesh* mesh);
+	void AddChildNode(Scene_Node* n);
+
+	bool DetachNode(Scene_Node* n);
+
+	void Execute(XMMATRIX* world, XMMATRIX* view, XMMATRIX* projection);
+
+private:
+	Mesh* m_pModel;
+	vector <Scene_Node*> m_children;
+
+	float m_x, m_y, m_z;
+	float m_xangle, m_yangle, m_zangle;
+	float m_scale;
+};
+
