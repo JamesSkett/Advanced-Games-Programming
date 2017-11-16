9bc1e055dc53bdfb3b3355f36cd1f4ab3d9dc3d5
James Skett <jamesskett31@gmail.com>
2017-11-16 19:49:52 +0000

    Added get scale and update pos functions

--- CGP600 AE2/Scene_Node.h
+++ CGP600 AE2/Scene_Node.h
@@ -22,6 +22,7 @@ public:
 	float GetXAngle();
 	float GetYAngle();
 	float GetZAngle();
+	float GetScale();
 
 	//Setters
 	void SetXPos(float x);
@@ -30,6 +31,11 @@ public:
 	void SetXAngle(float xAngle);
 	void SetYAngle(float yAngle);
 	void SetZAngle(float zAngle);
+	void SetScale(float scale);
+
+	void UpdateXPos(float distance);
+	void UpdateYPos(float distance);
+	void UpdateZPos(float distance);
 
 	void SetModel(Mesh* mesh);
 	void AddChildNode(Scene_Node* n);
