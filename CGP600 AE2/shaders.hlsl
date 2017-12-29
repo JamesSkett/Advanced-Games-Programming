8248272018b776a0e2797ac0ce1751a7f9ddad88
James Skett <jamesskett31@gmail.com>
2017-12-29 18:33:54 +0000

    updated shaders

--- CGP600 AE2/shaders.hlsl
+++ CGP600 AE2/shaders.hlsl
@@ -1,14 +1,10 @@
+cbuffer CB0
+{
+	matrix WVPMatrix; //64 bytes
+};
+
 Texture2D texture0;
 SamplerState sampler0;
-
-cbuffer CBuffer0
-{
-	matrix WVPMatrix; // 64 bytes
-	float4 directional_light_vector;	// 16 bytes
-	float4 directional_light_colour;	// 16 bytes
-	float4 ambient_light_colour;		// 16 bytes
-} // TOTAL SIZE = 112 bytes
-
 
 struct VOut
 {
@@ -17,14 +13,16 @@ struct VOut
 	float2 texcoord : TEXCOORD;
 };
 
-VOut VShader(float4 position : POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD, float3 normal : NORMAL)
+VOut VShader(float4 position : POSITION, float2 texcoord : TEXCOORD, float3 normal : NORMAL)
 {
 	VOut output;
-	output.position = mul(WVPMatrix, position);
 
-	float diffuse_amount = dot(directional_light_vector, normal);
-	diffuse_amount = saturate(diffuse_amount);
-	output.color = ambient_light_colour + (directional_light_colour * diffuse_amount);
+	float4 default_color = { 1,1,1,1 };
+
+	output.position = mul(WVPMatrix, position);
+	// diffuse_amount = dot(directional_light_vector, normal);
+	//diffuse_amount = saturate(diffuse_amount);
+	//output.color = ambient_light_colour + (directional_light_colour * diffuse_amount);
 
 	output.texcoord = texcoord;
 
@@ -33,5 +31,5 @@ VOut VShader(float4 position : POSITION, float4 color : COLOR, float2 texcoord :
 
 float4 PShader(float4 position : SV_POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD) : SV_TARGET
 {
-	return color * texture0.Sample(sampler0, texcoord);
+	return texture0.Sample(sampler0, texcoord) * color;
 }
