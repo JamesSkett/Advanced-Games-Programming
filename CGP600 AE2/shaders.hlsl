e59c9b0f45d1a4ad7534525dacaada1dbea5450a
James Skett <jamesskett31@gmail.com>
2017-12-31 00:47:17 +0000

    shader now loads a cube map

--- CGP600 AE2/shaders.hlsl
+++ CGP600 AE2/shaders.hlsl
@@ -3,14 +3,14 @@ cbuffer CB0
 	matrix WVPMatrix; //64 bytes
 };
 
-Texture2D texture0;
+TextureCube texture0;
 SamplerState sampler0;
 
 struct VOut
 {
 	float4 position : SV_POSITION;
 	float4 color : COLOR;
-	float2 texcoord : TEXCOORD;
+	float3 texcoord : TEXCOORD;
 };
 
 VOut VShader(float4 position : POSITION, float2 texcoord : TEXCOORD, float3 normal : NORMAL)
@@ -19,17 +19,17 @@ VOut VShader(float4 position : POSITION, float2 texcoord : TEXCOORD, float3 norm
 
 	float4 default_color = { 1,1,1,1 };
 
+	output.texcoord = position.xyz;
 	output.position = mul(WVPMatrix, position);
 	// diffuse_amount = dot(directional_light_vector, normal);
 	//diffuse_amount = saturate(diffuse_amount);
 	//output.color = ambient_light_colour + (directional_light_colour * diffuse_amount);
 
-	output.texcoord = texcoord;
 
 	return output;
 }
 
-float4 PShader(float4 position : SV_POSITION, float4 color : COLOR, float2 texcoord : TEXCOORD) : SV_TARGET
+float4 PShader(float4 position : SV_POSITION, float4 color : COLOR, float3 texcoord : TEXCOORD) : SV_TARGET
 {
 	return texture0.Sample(sampler0, texcoord) * color;
 }
