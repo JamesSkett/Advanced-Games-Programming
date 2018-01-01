ec71cf59279292e1d42f9560c04f1bd91d94fbdb
James Skett <jamesskett31@gmail.com>
2018-01-01 19:38:08 +0000

    updates to projectile class

--- CGP600 AE2/Projectile.cpp
+++ CGP600 AE2/Projectile.cpp
@@ -12,10 +12,12 @@ Projectile::~Projectile()
 {
 }
 
-void Projectile::UpdateProjectile(Scene_Node* root_node)
+bool Projectile::UpdateProjectile(Scene_Node* root_node)
 {
 	if (m_isFired)
 	{
+		m_isFired = false;
+
 		xyz startPos = { m_x, m_y, m_z };
 		xyz currentPos;
 		float distance;
@@ -23,14 +25,31 @@ void Projectile::UpdateProjectile(Scene_Node* root_node)
 		while(inRange)
 		{
 			currentPos = { m_x, m_y, m_z };
-			MoveForward(m_velocity, root_node);
+			MoveForward(m_velocity);
 
 			distance = sqrt(pow(currentPos.x - startPos.x, 2) + pow(currentPos.y - startPos.y, 2) + pow(currentPos.z - startPos.z, 2));
 
-			if (distance < 100.0f)
+			if (distance >= 100.0f)
 			{
-
+				m_x = 0.0f;
+				m_y = 0.0f;
+				m_z = 0.0f;
+				return false;
 			}
 		}
 	}
+}
+
+void Projectile::SetIsFired(bool isFired)
+{
+	m_isFired = isFired;
+}
+
+void Projectile::MoveForward(float speed)
+{
+
+	m_x += m_dx * speed;
+	m_y += -m_dy * speed;
+	m_z += m_dz * speed;
+
 }
--- CGP600 AE2/Projectile.h
+++ CGP600 AE2/Projectile.h
@@ -8,7 +8,10 @@ public:
 	Projectile(float velocity);
 	~Projectile();
 
-	void UpdateProjectile(Scene_Node* root_node);
+	bool UpdateProjectile(Scene_Node* root_node);
+	void SetIsFired(bool isFired);
+
+	void MoveForward(float speed);
 
 private:
 	bool m_isFired = false;
