03786bf30c673983141d8613a7f83f30edfab1d8
James Skett <jamesskett31@gmail.com>
2018-01-04 23:33:31 +0000

    added comments

--- CGP600 AE2/Planet.cpp
+++ CGP600 AE2/Planet.cpp
@@ -1,7 +1,9 @@
 #include "Planet.h"
 #include "Renderer.h"
 
-Planet::Planet(int health, float size, float xPos, float yPos, float zPos, char* textureFile)
+
+// Set up the planet properties
+Planet::Planet(int health, float size, float xPos, float yPos, float zPos)
 {
 	m_health = health;
 	m_scale = size;
@@ -15,7 +17,7 @@ Planet::Planet(int health, float size, float xPos, float yPos, float zPos, char*
 
 }
 
-
+//delete the object when done
 Planet::~Planet()
 {
 	if (healthText)
@@ -25,6 +27,7 @@ Planet::~Planet()
 	}
 }
 
+//updates the planet every frame
 void Planet::UpdatePlanet(Scene_Node* playerShip)
 {
 	UpdateYangle(0.5f);
@@ -35,23 +38,33 @@ void Planet::UpdatePlanet(Scene_Node* playerShip)
 
 
 	float distance = sqrt(pow(m_x - playerShip->GetXPos(), 2) + pow(m_y - playerShip->GetYPos(), 2) + pow(m_z - playerShip->GetZPos(), 2));
-	if (distance < 200)
+	
+	if (distance < 150) //display the health when within range
 	{
 		healthText->AddText(m_healthString, -0.5, -0.8, 0.04);
 	}
 
+	if (m_health <= 0) //planet is destryed dont render or check collision
+	{
+		m_canDraw = false;
+		m_canCollide = false;
+	}
+
 }
 
+//Renders the text when called
 void Planet::RenderText()
 {
 	healthText->RenderText();
 }
 
+//rotates the planet round the y axis
 void Planet::UpdateYangle(float angle)
 {
 	m_yangle += angle;
 }
 
+//removes health from the planet
 void Planet::RemoveHealth(int value)
 {
 	m_health -= value;
--- CGP600 AE2/Planet.h
+++ CGP600 AE2/Planet.h
@@ -6,15 +6,21 @@ class Planet :
 	public Scene_Node
 {
 public:
-	Planet(int health, float size, float xPos, float yPos, float zPos, char* textureFile);
+	// Set up the planet properties
+	Planet(int health, float size, float xPos, float yPos, float zPos);
+	//delete the object when done
 	~Planet();
 
+	//updates the planet every frame
 	void UpdatePlanet(Scene_Node* playerShip);
 
+	//Renders the text when called
 	void RenderText();
 
+	//rotates the planet round the y axis
 	void UpdateYangle(float angle);
 
+	//removes health from the planet
 	void RemoveHealth(int value);
 
 private:
